4f8ae3c2e801cf240e37286fa253bf36bf88a3d3
sarthakpatel <sr555161@dal.ca>
2023-12-03 20:17:26 -0400

    Amazon Migrate

---
+++ Amazon-Clone/amazon-backend/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs"
+
+export const users =[
+    {
+        name: "Debjit Pramanick",
+        email: "debjit1@gmail.com",
+        password: bcrypt.hashSync('1234',8),
+        isAdmin: true,
+    },
+    {
+        name: "Rohan Das",
+        email: "rohan67@gmail.com",
+        password: bcrypt.hashSync('1234',8),
+        isAdmin: false,
+    },
+    {
+        name: "Sarthak Patel",
+        email: "sarthak3136@gmail.com",
+        password: bcrypt.hashSync('1234',8),
+        isAdmin: true
+    }
+]
